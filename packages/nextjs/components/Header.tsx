f07222733d5ebe6aed7c5b643b01719c3b9e1cc0
Nicolas <perfectride@protonmail.com>
2024-01-18 19:21:57 +0100

    Added DeFi Conquest name

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -93,8 +93,8 @@ export const Header = () => {
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">DeFi Conquest</span>
+            <span className="text-xs">Bringing fun to DeFi!</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
