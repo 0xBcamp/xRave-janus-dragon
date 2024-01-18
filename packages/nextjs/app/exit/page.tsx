d641615e2a0bf1b6fb19627b31a886587f578cd8
Nicolas <perfectride@protonmail.com>
2024-01-18 23:08:27 +0100

    Added withdrawal button to Exit page

--- packages/nextjs/app/exit/page.tsx
+++ packages/nextjs/app/exit/page.tsx
@@ -1,6 +1,18 @@
+"use client";
+
 import type { NextPage } from "next";
+import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
 
 const Exit: NextPage = () => {
+  const { writeAsync } = useScaffoldContractWrite({
+    contractName: "YourContract",
+    functionName: "withdraw",
+    blockConfirmations: 1,
+    onBlockConfirmation: txnReceipt => {
+      console.log("Transaction blockHash", txnReceipt.blockHash);
+    },
+  });
+
   return (
     <>
       <div className="flex items-center flex-col flex-grow pt-10">
@@ -9,7 +21,9 @@ const Exit: NextPage = () => {
             <span className="block text-2xl mb-2">Withdraw from the pool</span>
             <span className="block text-4xl font-bold">and get your rewards</span>
           </h1>
-          <button className="btn btn-secondary">Withdraw</button>
+          <button className="btn btn-secondary" onClick={() => writeAsync()}>
+            Withdraw
+          </button>
         </div>
       </div>
     </>
