3845520dcf81e9bd328c6ed8a56087ba3330d0ca
Nicolas <perfectride@protonmail.com>
2024-01-20 13:35:15 +0100

    Added tournament page

--- packages/nextjs/components/tournament/item.tsx
+++ packages/nextjs/components/tournament/item.tsx
@@ -4,7 +4,6 @@ export const Item = ({ tournament }: { tournament: Tournaments }) => {
   return (
     <li key={tournament.id} className="flex justify-between gap-x-6 py-5">
       <div className="flex min-w-0 gap-x-4">
-        <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src="" alt="" />
         <div className="min-w-0 flex-auto">
           <p className="text-sm font-semibold leading-6 text-gray-900">{tournament.name}</p>
           <p className="mt-1 truncate text-xs leading-5 text-gray-500">{tournament.LPTokenSymbol}</p>
