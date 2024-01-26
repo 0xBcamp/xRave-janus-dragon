62773ba9265e47ff791dc142a5722cd795794234
Nicolas <perfectride@protonmail.com>
2024-01-26 21:25:59 +0100

    Added comments

--- packages/hardhat/contracts/Tournament.sol
+++ packages/hardhat/contracts/Tournament.sol
@@ -18,23 +18,20 @@ contract UniswapInterface {
 contract Tournament {
 	// State Variables
 	address public immutable owner;
-	// mapping(address => uint256) public playerToLPToken; // how much LP token each player has deposited
 	mapping(address => uint256) public playerToScore; // how many points each player has
-	mapping(address => uint256) public playerToLastGame; // when the player last played
+	mapping(address => uint256) public playerToLastGame; // when the player last played (used to determine if the player already played today)
 	mapping(address => uint256) public playerToDepositPricePerShare; // price per share at deposit
-	mapping(address => uint256) public playerToDepositPricePerShare2; // price per share at deposit
+	mapping(address => uint256) public playerToDepositPricePerShare2; // price per share at deposit (only used for UniswapV2 LPs)
 	address[] public players;
-	// uint256 public numPlayers;
-	string public name;
+	string public name; // Name of the tournament
 	uint256 public contractLPToken; // amount of LP token held by the contract
 	IERC20Metadata poolIncentivized;
 	string public LPTokenSymbol;
-	uint256 public LPTokenAmount;
+	uint256 public LPTokenAmount; // Amount of LP to be deposited by the players
 	uint256 public startTime;
 	uint256 public endTime;
-	uint256 public totalUnconvertedScore;
-	uint256 realizedPoolPrize;
-	uint256 realizedFees;
+	uint256 realizedPoolPrize; // Amount of LP left by players that withdrawn
+	uint256 realizedFees; // Amount of LP fees left by players that withdrawn
 	uint256 unclaimedPoolPrize = 1 ether; // 100% of the pool prize unclaimed
 	Protocol public protocol;
 	uint256 public fees = 0.1 ether; // 10% fees on pool prize
@@ -44,6 +41,12 @@ contract Tournament {
 	enum Protocol {
 		Uniswap,
 		Yearn
+	}
+
+	enum Moves {
+		Paper,
+		Rock,
+		Scissors
 	}
 
 	// Events: a way to emit log statements from smart contract that can be listened to by external parties
@@ -101,12 +104,10 @@ contract Tournament {
 	 */
 	function stakeLPToken() public {
 		require(IERC20(poolIncentivized).transferFrom(msg.sender, address(this), LPTokenAmount), "Transfer of LP token Failed");
-		// playerToLPToken[msg.sender] += LPTokenAmount;
 		(uint256 pPS, uint256 pPS2) = getPricePerShare();
 		playerToDepositPricePerShare[msg.sender] = pPS;
 		playerToDepositPricePerShare2[msg.sender] = pPS2;
 		players.push(msg.sender);
-		// numPlayers += 1;
 		// emit: keyword used to trigger an event
 		emit Staked(msg.sender, LPTokenAmount);
 	}
@@ -115,13 +116,15 @@ contract Tournament {
 	 * Function that allows anyone to unstake their LP token once the tournament is over
 	 */
 	function unstakeLPToken() public {
+		// Get back its deposited value of underlying assets
 		uint256 amount = LPTokenAmountOfPlayer(_player); // corresponds to deposited underlying assets
-		uint256 extraPoolPrize = (1 ether - fees) / 1 ether * (LPTokenAmount - amount);
+		uint256 extraPoolPrize = (1 ether - fees) / 1 ether * (LPTokenAmount - amount); // How much LP token is left by the user
 		realizedPoolPrize += extraPoolPrize;
 		realizedFees += LPTokenAmount - extraPoolPrize;
-		// rewards from the game
-		amount += getPoolPrize() * getRewardShare(_player) / 1 ether;
-		// playerToLPToken[msg.sender] = 0;
+		// Add rewards from the game
+		uint share = getRewardShare(_player);
+		amount += getPoolPrize() * share / 1 ether;
+		unclaimedPoolPrize -= share; // TODO: useful?
 		require(IERC20(poolIncentivized).transfer(msg.sender, amount), "Transfer of LP token Failed");
 
 		playerToDepositPricePerShare[msg.sender] = 0; // Reuse of this variable to indicate that the player unstaked its LP token
@@ -129,6 +132,11 @@ contract Tournament {
 		emit Unstaked(msg.sender, amount);
 	}
 
+	/**
+	 * Function that allows the owner to withdraw realized fees
+	 * Total fees will be available for withdrawal once all players have withdrawn
+	 * Partial fees can be withdran at any time after players begun to withdraw
+	 */
 	function withdrawFees() public onlyOwner {
 		require(IERC20(poolIncentivized).transfer(msg.sender, realizedFees), "Transfer of LP token Failed");		
 		realizedFees = 0;
@@ -151,9 +159,15 @@ contract Tournament {
 	 */
 	function playAgainstPlayer(string memory _move) public {
 		playerToLastGame[msg.sender] = now;
+		updateScore(msg.sender, 1); // TODO: game logic
 	}
 
+	/**
+	 * Function that updates the player score by adding the points
+	 */
 	function updateScore(address, _player, uint256 _points) internal {
+		if(_points == 0) { return; }
+		// We first remove the player from it's current rank
 		for(uint i=0; i<scoreToPlayers[playerToScore[_player]].length; i++) {
 			if(scoreToPlayers[playerToScore[_player]][i] == _player) {
 				scoreToPlayers[playerToScore[_player]][i] = scoreToPlayers[playerToScore[_player]][scoreToPlayers[playerToScore[_player]].length - 1];
@@ -161,6 +175,7 @@ contract Tournament {
 			}
 		}
 		scoreToPlayers[playerToScore[_player]].pop();
+		// Now we can update the score and push the user to its new rank
 		playerToScore[_player] += _points;
 		if(topScore < playerToScore[_player]) {
 			topScore = playerToScore[_player];
@@ -184,19 +199,27 @@ contract Tournament {
 	 * Function that returns the player's reward share (50% shared for 1st rank, 25% shared for 2nd rank, etc)
 	 */
 	function getRewardShare(address _player) public view returns (uint256) {
+		// TODO: how to manage rewards if the number of different ranks is low?
 		(uint256 rank, uint256 split) = getRank();
 		return (1 ether / (2 ** rank)) / split;
 	}
 
+	/**
+	 * Function that returns the current price per share from the LP token contract
+	 */
 	function getPricePerShare() private returns(uint256, uint256) {
 		if(Protocol.Yearn == protocol) {
 			YearnInterface yearn = YearnInterface(poolIncentivized);
 			return ( yearn.pricePerShare(), 0 );
 		} else { // Uniswap
+			UniswapInterface uniswap = UniswapInterface(poolIncentivized);
 			return ( uniswap.getReserves()[0] / uniswap.totalSupply(), uniswap.getReserves()[1] / uniswap.totalSupply() );
 		}
 	}
 
+	/**
+	 * Function that returns the amount of LP token in the pool prize
+	 */
 	function getPoolPrize() public view returns (uint256) {
 		uint256 extraLP = 0;
 		for (uint i=0; i<players.length; i++) {
@@ -206,10 +229,16 @@ contract Tournament {
 		return realizedPoolPrize + extraLP * (1 ether - fees) / 1 ether;
 	}
 
+	/**
+	 * Function that returns the expected pool prize at the end of the tournament from the accrued LP since the start
+	 */
 	function getExpectedPoolPrize() public view returns (uint256) {
 		return getPoolPrize() * (endTime - startTime) / (now - startTime);
 	}
 
+	/**
+	 * Function that returns the amount of fees accrued by the protocol on this tournament
+	 */
 	function getFees() public view returns (uint256) {
 		return fees / 1 ether * getPoolPrize();
 	}
@@ -218,6 +247,9 @@ contract Tournament {
 		return players.length;
 	}
 
+	/**
+	 * Function that returns if the tournament is active (players are allowed to play)
+	 */
 	function isActive() public view returns (bool) {
 		return now >= startTime && now < endTime;
 	}
@@ -226,17 +258,21 @@ contract Tournament {
 		return now >= endTime;
 	}
 
+	/**
+	 * Function that returns true if the tournament is not yet started
+	 */
 	function isFuture() public view returns (bool) {
 		return now < startTime;
 	}
 
 	function isPlayer(address _player) public view returns (bool) {
-		return playerToLPToken[_player] > 0;
+		return playerToDepositPricePerShare[_player] > 0;
 	}
 
+	/**
+	 * Function that returns if the player has already played today (resets at O0:OO UTC)
+	 */
 	function alreadyPlayed(address _player) public view returns (bool) {
-		// return playerToLastGame[_player] + 1 days > now;
-		// TODO: modify to find the timestamp at 0:00 UTC (timestamp divided by 60/60/24 should be an integer)
 		uint256 today = ( now - ( now % (60 * 60 * 24) ) ) / (60 * 60 * 24);
 		uint256 lastGame = ( playerToLastGame[_player] - ( playerToLastGame[_player] % (60 * 60 * 24) ) ) / (60 * 60 * 24);
 		return today == lastGame;
@@ -246,6 +282,9 @@ contract Tournament {
 		return playerToScore[_player];
 	}
 
+	/**
+	 * Function that returns the current amount of LP token entitled to the player on withdrawal (before adding earned prizes)
+	 */
 	function LPTokenAmountOfPlayer(address _player) public view returns (uint256) {
 		if(Protocol.Yearn == protocol) {
 			// return playerToLPToken[_player] * playerToDepositPricePerShare[_player] / getPricePerShare()[0];
