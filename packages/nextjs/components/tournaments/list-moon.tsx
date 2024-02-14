bf94d393fb6e29725090ce01188b4ee69975abba
Nicolas <perfectride@protonmail.com>
2024-02-14 17:35:29 +0100

    Added list-moon

--- packages/nextjs/components/tournaments/list-moon.tsx
+++ packages/nextjs/components/tournaments/list-moon.tsx
@@ -1,36 +1,51 @@
+import { useEffect, useState } from "react";
 import { Item } from "./item";
-import { useAccount } from "wagmi";
+import { MoonProvider } from "@moonup/ethers";
+import { AUTH, MOON_SESSION_KEY, Storage } from "@moonup/moon-types";
+import { ethers } from "ethers";
 import { InformationCircleIcon } from "@heroicons/react/24/outline";
-import { useScaffoldContractRead } from "~~/hooks/scaffold-eth";
+import DeployedContracts from "~~/contracts/deployedContracts";
 
 export const List = () => {
-  const { data: activeTournaments, isLoading: isActiveTournamentsLoading } = useScaffoldContractRead({
-    contractName: "TournamentFactory",
-    functionName: "getAllActiveTournaments",
+  const [loaded, setLoaded] = useState(false);
+  const [activeTournaments, setActiveTournaments] = useState([] as string[]);
+  const [futureTournaments, setFutureTournaments] = useState([] as string[]);
+  const [pastTournaments, setPastTournaments] = useState([] as string[]);
+  const [playerTournaments, setPlayerTournaments] = useState([] as string[]);
+
+  const provider = new MoonProvider({
+    chainId: 31337,
+    MoonSDKConfig: {
+      Storage: {
+        key: MOON_SESSION_KEY,
+        type: Storage.SESSION,
+      },
+      Auth: {
+        AuthType: AUTH.JWT,
+      },
+    },
   });
 
-  const { data: futureTournaments, isLoading: isFutureTournamentsLoading } = useScaffoldContractRead({
-    contractName: "TournamentFactory",
-    functionName: "getAllFutureTournaments",
-  });
+  const tournamentFactory = new ethers.Contract(
+    DeployedContracts[31337].Tournament.address,
+    DeployedContracts[31337].Tournament.abi,
+    provider,
+  );
 
-  const { data: pastTournaments, isLoading: isPastTournamentsLoading } = useScaffoldContractRead({
-    contractName: "TournamentFactory",
-    functionName: "getAllPastTournaments",
-  });
+  useEffect(() => {
+    const getTournaments = async () => {
+      setActiveTournaments(await tournamentFactory.getAllActiveTournaments());
+      setFutureTournaments(await tournamentFactory.getAllFutureTournaments());
+      setPastTournaments(await tournamentFactory.getAllPastTournaments());
+      setPlayerTournaments(await tournamentFactory.getAllPlayerTournaments());
+    };
+    if (!loaded) {
+      getTournaments();
+      setLoaded(true);
+    }
+  }, []);
 
-  const { data: playerTournaments, isLoading: isPlayerTournamentsLoading } = useScaffoldContractRead({
-    contractName: "TournamentFactory",
-    functionName: "getTournamentsByPlayer",
-    args: [useAccount()?.address],
-  });
-
-  if (
-    isActiveTournamentsLoading ||
-    isFutureTournamentsLoading ||
-    isPastTournamentsLoading ||
-    isPlayerTournamentsLoading
-  ) {
+  if (!loaded) {
     return <div className="flex justify-center items-center mt-10">Loading...</div>;
   }
 
@@ -38,7 +53,7 @@ export const List = () => {
 
   return (
     <>
-      {(activeTournaments?.length || 0) + (futureTournaments?.length || 0) + (pastTournaments?.length || 0) === 0 ? (
+      {activeTournaments?.length + futureTournaments?.length + pastTournaments?.length === 0 ? (
         <div className="flex justify-center items-center mt-10">
           <div
             className="flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
